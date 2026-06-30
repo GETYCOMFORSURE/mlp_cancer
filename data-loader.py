de2984924c4c4e223b2ee950c087157b9ecaef28
Siyun <siyun_cao@outlook.com>
2026-06-30 22:30:48 +0800

    normalisation in data-loader.py

--- data-loader.py
+++ data-loader.py
@@ -1,10 +1,21 @@
 from sklearn.datasets import load_breast_cancer
 
 data = load_breast_cancer()
-X = data.data      # shape (569, 30) — the features
-y = data.target    # shape (569,) — the labels (0 or 1)
+X = data.data
+y = data.target
 
 print(X.shape)
 print(y.shape)
-print(X[0])         # one actual sample, 30 numbers
-print(y[0])          # its label
+print(X[0])         
+print(y[0])          
+
+# formula for normalization: X_normalized = (X - mean(X)) / std(X)
+X_normalized = []
+for x in X:
+  a = x - mean_x
+  x_normalized = a / std_X
+  X_normalized.append(x_normalized)
+
+
+  
+
