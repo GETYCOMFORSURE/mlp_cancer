5e4bd02dc3b1c6a3728648f068e797e836af113e
Siyun <siyun_cao@outlook.com>
2026-06-30 22:41:43 +0800

    Implement feature normalization and data splitting
    Added normalization of features and train/test split.

--- data-loader.py
+++ data-loader.py
@@ -1,4 +1,6 @@
+import numpy as np
 from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
 
 data = load_breast_cancer()
 X = data.data
@@ -12,10 +14,14 @@ print(y[0])
 # formula for normalization: X_normalized = (X - mean(X)) / std(X)
 X_normalized = []
 for x in X:
+  mean_x = np.mean(X, axis=i)
+  std_X = np.std(X, axis=i)
   a = x - mean_x
   x_normalized = a / std_X
   X_normalized.append(x_normalized)
 
-
-  
-
+# split into train / test
+X_shuffle = np.random.permutation(X_normalized)
+split = int(0.6 * len(X_shuffle))
+X_train = X[:split] # 60% data used for train
+X_test = X[split:]
