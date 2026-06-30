ae5d72e07701ebcf8de9d2cc5468f49e02cb56a3
Siyun <siyun_cao@outlook.com>
2026-06-30 23:04:39 +0800

    debug data-loader.py

--- data-loader.py
+++ data-loader.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 
+# take a glimpse of the data
 data = load_breast_cancer()
 X = data.data
 y = data.target
@@ -9,19 +10,32 @@ y = data.target
 print(X.shape)
 print(y.shape)
 print(X[0])         
-print(y[0])          
+print(y[0])     
+print(data.feature_names)
 
 # formula for normalization: X_normalized = (X - mean(X)) / std(X)
 X_normalized = []
+mean_x = np.mean(X, axis=0) # axis=0 means "collapse going down the rows, for each column separately" 
+std_X = np.std(X, axis=0)
 for x in X:
-  mean_x = np.mean(X, axis=i)
-  std_X = np.std(X, axis=i)
   a = x - mean_x
   x_normalized = a / std_X
   X_normalized.append(x_normalized)
+print(mean_x)
+print(std_X)
+print(X_normalized[0])
 
 # split into train / test
-X_shuffle = np.random.permutation(X_normalized)
-split = int(0.6 * len(X_shuffle))
-X_train = X[:split] # 60% data used for train
-X_test = X[split:]
+indices = np.random.permutation(len(X_normalized)) # make sure shuffled x has corresponding y
+X_shuffle = np.array(X_normalized)[indices]
+y_shuffle = y[indices]
+split = int(0.6 * len(X_shuffle)) # 60% data used for train
+X_train = X_shuffle[:split]
+X_test = X_shuffle[split:]
+y_train = y_shuffle[:split]
+y_test = y_shuffle[split:]
+
+print(X_train.shape)
+print(X_test.shape)
+print(y_train.shape)
+print(y_test.shape)
